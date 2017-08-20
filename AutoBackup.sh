fb6143cc582248af221b829adb814f9fd8898f88
zero <lizihevip@163.com>
2017-08-21 02:07:15 +0800

    Auto Backuping on 20170821

--- AutoBackup.sh
+++ AutoBackup.sh
@@ -4,6 +4,8 @@ cp ~/.vimrc ~/backup/backup/
 cp ~/notes ~/backup/backup/
 cp ~/backup/AutoBackup.sh ~/backup/backup/
 cp /etc/fstab ~/backup/backup
+cp /etc/udev/rules.d/90-android-tethering.rules ~/90-android-tethering.rules
+cp /etc/systemd/network/50-enp0s20u2.network ~/50-enp0s20u2.network
 cd ~/backup/backup
 git add .
 date=`date +%Y%m%d`
