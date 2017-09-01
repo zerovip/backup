60185557be47f5d11d47ba7ae03ed5584fea5d03
zero <lizihevip@163.com>
2017-09-02 01:46:45 +0800

    Auto Backuping on 20170902

--- AutoBackup.sh
+++ AutoBackup.sh
@@ -4,8 +4,8 @@ cp ~/.vimrc ~/backup/backup/
 cp ~/notes ~/backup/backup/
 cp ~/backup/AutoBackup.sh ~/backup/backup/
 cp /etc/fstab ~/backup/backup
-cp /etc/udev/rules.d/90-android-tethering.rules ~/backup/backup/
-cp /etc/systemd/network/50-enp0s20u2.network ~/backup/backup/
+###  cp /etc/udev/rules.d/90-android-tethering.rules ~/backup/backup/
+###  cp /etc/systemd/network/50-enp0s20u2.network ~/backup/backup/
 cd ~/backup/backup
 git add .
 date=`date +%Y%m%d`
