c68582193cba27da00964ee645720a4bd6ab733d
zero <lizihevip@163.com>
2017-11-25 00:25:28 +0800

    Auto Backuping on 20171125

--- AutoBackup.sh
+++ AutoBackup.sh
@@ -6,6 +6,9 @@ cp ~/backup/AutoBackup.sh ~/backup/backup/
 cp /etc/fstab ~/backup/backup
 ###  cp /etc/udev/rules.d/90-android-tethering.rules ~/backup/backup/
 ###  cp /etc/systemd/network/50-enp0s20u2.network ~/backup/backup/
+pacman -Qeq > ~/backup/backup/alpkglist
+pacman -Qmq > ~/backup/backup/otpkglist
+comm -23 ~/backup/backup/alpkglist ~/backup/backup/otpkglist > ~/backup/backup/pkglist
 cd ~/backup/backup
 git add .
 date=`date +%Y%m%d`
