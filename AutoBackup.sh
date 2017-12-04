9c63a4152a1938135b3bc0facce4b6e7cd8a3d39
zero <lizihevip@163.com>
2017-12-04 23:41:37 +0800

    Auto Backuping on 20171204

--- AutoBackup.sh
+++ AutoBackup.sh
@@ -3,12 +3,13 @@ cp ~/.zshrc ~/backup/backup/
 cp ~/.vimrc ~/backup/backup/
 cp ~/notes ~/backup/backup/
 cp ~/backup/AutoBackup.sh ~/backup/backup/
-cp /etc/fstab ~/backup/backup
+cp /etc/fstab ~/backup/backup/
 ###  cp /etc/udev/rules.d/90-android-tethering.rules ~/backup/backup/
 ###  cp /etc/systemd/network/50-enp0s20u2.network ~/backup/backup/
 pacman -Qeq | sort > ~/backup/backup/alpkglist
 pacman -Qmq | sort > ~/backup/backup/otpkglist
 comm -23 ~/backup/backup/alpkglist ~/backup/backup/otpkglist > ~/backup/backup/pkglist
+cp ~/.mozilla/firefox/3cky76on.default/chrome/userChrome.css ~/backup/backup/
 cd ~/backup/backup
 git add .
 date=`date +%Y%m%d`
