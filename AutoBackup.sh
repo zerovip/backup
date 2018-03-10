e91937b020eb7066d5ea0f9288e7f166369e93b5
zero <lizihevip@163.com>
2018-03-11 01:14:57 +0800

    Auto Backuping on 20180311

--- AutoBackup.sh
+++ AutoBackup.sh
@@ -4,6 +4,7 @@ cp ~/.vimrc ~/backup/backup/
 cp ~/notes ~/backup/backup/
 cp ~/backup/AutoBackup.sh ~/backup/backup/
 cp /etc/fstab ~/backup/backup/
+cp ~/.config/ibus/rime/default.custom.yaml ~/backup/backup/
 ###  cp /etc/udev/rules.d/90-android-tethering.rules ~/backup/backup/
 ###  cp /etc/systemd/network/50-enp0s20u2.network ~/backup/backup/
 pacman -Qeq | sort > ~/backup/backup/alpkglist
